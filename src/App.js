1090c385b6c562d120aeda65cad8f5b1da933c84
batraz <baticpro@gmail.com>
2020-03-24 12:13:26 +0300

    add lint-staged

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { hot } from 'react-hot-loader/root';
 import React from 'react';
-import Test from "./components/test";
+import Test from './components/test';
 
 const App = () => {
   return (
     <div>
       Welcome to Batraz's web-site
-      <Test/>
+      <Test />
     </div>
   );
 };

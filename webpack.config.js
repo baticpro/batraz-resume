d4d304ecb39edc3df1514a3800d203417bdb3fae
batraz <baticpro@gmail.com>
2020-03-24 11:51:19 +0300

    add linter

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,7 @@ const webpack = require("webpack");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 const htmlPlugin = new HtmlWebPackPlugin({
-  template: "./src/index.html",
-  //filename: "../index.html"
+  template: "./src/index.html"
 });
 
 const hotReloadPlugin = new webpack.HotModuleReplacementPlugin();

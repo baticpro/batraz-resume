9f525f9a13625d1c9b69254a3ffe78411d70c0bc
batraz <baticpro@gmail.com>
2020-03-31 14:42:56 +0300

    ready resume ui

--- src/components/content/blocks/resume.js
+++ src/components/content/blocks/resume.js
@@ -15,7 +15,7 @@ const Resume = () => (
     </p>
 
     <div className="flex-grid">
-      <div className="flex-50 working-stamp">
+      <div className="flex-60 working-stamp">
         <h4>Experience</h4>
 
         <div className="working-item">
@@ -33,9 +33,24 @@ const Resume = () => (
             </p>
           </div>
         </div>
+        <div className="working-item">
+          <div className="working-meta">
+            <strong>2008 - current</strong>
+            <br />
+            <span>Jetruby Agancy</span>
+          </div>
+          <div className="working-position">
+            <h5>React Native Developer</h5>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi at eius enim expedita
+              explicabo non ratione? Ab blanditiis cum cumque dolorem exercitationem iste laudantium
+              perferendis quidem reprehenderit sequi tempora, voluptatibus.
+            </p>
+          </div>
+        </div>
       </div>
 
-      <div className="flex-50 skills-progress">
+      <div className="flex-40 skills-progress">
         <h4>
           My <span>Skills</span>
         </h4>
@@ -43,10 +58,27 @@ const Resume = () => (
         <div className="skill-bar">
           <div className="skill-meta">
             <div className="flex-grid">
-              <div className="flex-50">React</div>
-              <div className="flex-50">98%</div>
+              <div className="flex-50 skill-name">React</div>
+              <div className="flex-50 skill-percentage">98%</div>
             </div>
-
+            <ProgressBar value={98} />
+          </div>
+        </div>
+        <div className="skill-bar">
+          <div className="skill-meta">
+            <div className="flex-grid">
+              <div className="flex-50 skill-name">React</div>
+              <div className="flex-50 skill-percentage">98%</div>
+            </div>
+            <ProgressBar value={98} />
+          </div>
+        </div>
+        <div className="skill-bar">
+          <div className="skill-meta">
+            <div className="flex-grid">
+              <div className="flex-50 skill-name">React</div>
+              <div className="flex-50 skill-percentage">98%</div>
+            </div>
             <ProgressBar value={98} />
           </div>
         </div>

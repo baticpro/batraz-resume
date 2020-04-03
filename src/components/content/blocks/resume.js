d3df6c196cec27b5cf0872bfb46bf17e43d93cef
batraz <baticpro@gmail.com>
2020-04-03 18:27:48 +0300

    add resume information

--- src/components/content/blocks/resume.js
+++ src/components/content/blocks/resume.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './resume.scss';
 import ProgressBar from './ui/progress-bar';
+import { MY_EXPERIENCE, MY_SKILLS } from '../../../config';
 
 const Resume = () => (
   <div className="content-container resume">
@@ -9,45 +10,27 @@ const Resume = () => (
     </h3>
 
     <p className="resume-description">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi blanditiis dolor ducimus
-      eligendi ipsa, libero, nemo qui quibusdam quis soluta temporibus voluptas! Alias amet maxime
-      nihil, tempore temporibus totam veniam?
+      On this section You can find out information about my experience and skills. I like
+      technologies and always try extend my knowledge in different platforms.
     </p>
 
     <div className="flex-grid">
       <div className="flex-60 working-stamp">
         <h4>Experience</h4>
 
-        <div className="working-item">
-          <div className="working-meta">
-            <strong>2008 - current</strong>
-            <br />
-            <span>Jetruby Agancy</span>
+        {MY_EXPERIENCE.map((item) => (
+          <div key={item.date} className="working-item">
+            <div className="working-meta">
+              <strong>{item.date}</strong>
+              <br />
+              <span>{item.company}</span>
+            </div>
+            <div className="working-position">
+              <h5>{item.position}</h5>
+              <p>{item.description}</p>
+            </div>
           </div>
-          <div className="working-position">
-            <h5>React Native Developer</h5>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi at eius enim expedita
-              explicabo non ratione? Ab blanditiis cum cumque dolorem exercitationem iste laudantium
-              perferendis quidem reprehenderit sequi tempora, voluptatibus.
-            </p>
-          </div>
-        </div>
-        <div className="working-item">
-          <div className="working-meta">
-            <strong>2008 - current</strong>
-            <br />
-            <span>Jetruby Agancy</span>
-          </div>
-          <div className="working-position">
-            <h5>React Native Developer</h5>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi at eius enim expedita
-              explicabo non ratione? Ab blanditiis cum cumque dolorem exercitationem iste laudantium
-              perferendis quidem reprehenderit sequi tempora, voluptatibus.
-            </p>
-          </div>
-        </div>
+        ))}
       </div>
 
       <div className="flex-40 skills-progress">
@@ -55,33 +38,17 @@ const Resume = () => (
           My <span>Skills</span>
         </h4>
 
-        <div className="skill-bar">
-          <div className="skill-meta">
-            <div className="flex-grid">
-              <div className="flex-50 skill-name">React</div>
-              <div className="flex-50 skill-percentage">98%</div>
+        {MY_SKILLS.map((item) => (
+          <div key={item.name} className="skill-bar">
+            <div className="skill-meta">
+              <div className="flex-grid">
+                <div className="flex-50 skill-name">{item.name}</div>
+                <div className="flex-50 skill-percentage">{item.value}%</div>
+              </div>
+              <ProgressBar value={item.value} />
             </div>
-            <ProgressBar value={98} />
           </div>
-        </div>
-        <div className="skill-bar">
-          <div className="skill-meta">
-            <div className="flex-grid">
-              <div className="flex-50 skill-name">React</div>
-              <div className="flex-50 skill-percentage">98%</div>
-            </div>
-            <ProgressBar value={98} />
-          </div>
-        </div>
-        <div className="skill-bar">
-          <div className="skill-meta">
-            <div className="flex-grid">
-              <div className="flex-50 skill-name">React</div>
-              <div className="flex-50 skill-percentage">98%</div>
-            </div>
-            <ProgressBar value={98} />
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   </div>

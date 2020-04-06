1758ae3796ee104e0cf82582b19b9526ded95cea
batraz <baticpro@gmail.com>
2020-04-06 16:45:55 +0300

    add babel-polyfill

--- webpack.common.js
+++ webpack.common.js
@@ -6,7 +6,7 @@ const htmlPlugin = new HtmlWebPackPlugin({
 });
 
 module.exports = {
-  entry: './src/index.js',
+  entry: ['babel-polyfill', './src/index.js'],
   output: {
     path: path.join(__dirname, 'public/dist'),
     filename: 'bundle.js',
